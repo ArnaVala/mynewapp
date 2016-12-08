de160eddf5fd15dec19bff85f51d299c6406ccb5
ArnaVala <arnavala@gmail.com>
2016-12-09 01:43:56 +0200

    4.3 Controllers - orders created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "Bike", :description => "Colourful bicycle..", :image_url => "http://www.publicdomainpictures.net/pictures/210000/velka/colorful-bicycle.jpg", :colour => "Pink", :price => "30")
