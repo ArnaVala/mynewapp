d9002b4a8b926294c493e2bbc9081d76d8392ba4
ArnaVala <arnavala@gmail.com>
2016-12-10 22:03:32 +0200

    5.2 Forms/search & styling

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  #root 'static_pages#landing_page'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+  #root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
