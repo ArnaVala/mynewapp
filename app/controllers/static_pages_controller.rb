2d2627b65120e4a61695907565b2d9df14abe5a8
ArnaVala <arnavala@gmail.com>
2016-12-11 15:32:30 +0200

    Contact page, emails, sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,15 @@ class StaticPagesController < ApplicationController
   	@featured_product = Product.first
   	@products = Product.limit(4)
 	end
-	
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'arnavala@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  end
+
 end
