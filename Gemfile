98789e9f8a40448e3838739ffc7aae85395a71b5
ArnaVala <arnavala@gmail.com>
2016-12-12 08:54:34 +0200

    5.5 Authorization and notices

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 ruby '2.3.0'
 
+gem 'cancancan', '~> 1.10'
+#Devise for users
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'static_pages#landing_page'
 
   get 'static_pages/about'
 
@@ -12,12 +14,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  root 'static_pages#landing_page'
-  #root 'static_pages#index'
-
   post 'static_pages/thank_you'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  #DEVISE_for :USERS
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
