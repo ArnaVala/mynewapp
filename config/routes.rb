37db35ba598caece740c32974c90277e6be9bfb9
ArnaVala <arnavala@gmail.com>
2016-12-12 19:10:56 +0200

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-
-  root 'static_pages#landing_page'
 
   get 'static_pages/about'
 
@@ -14,10 +12,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+
   post 'static_pages/thank_you'
 
-  #DEVISE_for :USERS
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  root 'static_pages#landing_page'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
