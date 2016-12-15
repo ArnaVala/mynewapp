dad77059a9f16ca7c295aae21e247175205ac084
ArnaVala <arnavala@gmail.com>
2016-12-15 17:46:33 +0200

    6.5 stripe :key issue

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
-	Rails.configuration.stripe = {
-		:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-		:secret_key => ENV['STRIPE_SECRET_KEY']
-	}
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+  }
 else
-	Rails.configuration.stripe = {
-		:publishable_key => 'pk_test_NpIU5cYDvAoV27cwKFWlmvt6'
-		:secret_key =>  'sk_test_v7ZS353RvmUHkGwnB34QwU3q'
-	}
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_NpIU5cYDvAoV27cwKFWlmvt6',
+    :secret_key => 'sk_test_v7ZS353RvmUHkGwnB34QwU3q'
+  }
 end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
